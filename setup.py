3a37b880656f4f27010e77266be9f64ea5d181b5
Aleksandr Zuev <zuev08@gmail.com>
2021-02-20 21:03:47 +0100

    Increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="simple-mapreduce",
-    version="0.0.2",
+    version="0.0.3",
     author="Aleksandr Zuev",
     author_email="zuev08@gmail.com",
     description="Simple map-reduce implementation with no dependencies",
